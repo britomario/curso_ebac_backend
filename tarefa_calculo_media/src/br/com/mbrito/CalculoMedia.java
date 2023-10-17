78d27d8fc5d51ee99bcf8f729505d2668f982875
britomario <mario.rb@outlook.com>
2023-10-17 19:32:39 -0300

    calculo de media usando scanner

--- tarefa_calculo_media/src/br/com/mbrito/CalculoMedia.java
+++ tarefa_calculo_media/src/br/com/mbrito/CalculoMedia.java
@@ -1,15 +1,28 @@
 package br.com.mbrito;
 
+import java.util.Scanner;
+
 public class CalculoMedia {
 
     public static void main(String[] args) {
-        double nota1 = 10;
-        double nota2 = 5.4;
-        double nota3 = 4.6;
-        double nota4 = 8.4;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a nota 1: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.print("Digite a nota 2: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.print("Digite a nota 3: ");
+        double nota3 = scanner.nextDouble();
+
+        System.out.print("Digite a nota 4: ");
+        double nota4 = scanner.nextDouble();
 
         double media = (nota1 + nota2 + nota3 + nota4) / 4;
 
         System.out.println("A média é: " + media);
+
+        scanner.close();
     }
 }
